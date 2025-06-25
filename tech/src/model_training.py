ce29f0d9975f69df6dc79ac1fe2207a585237f8a
Himanshu Verma <himanshu.verma.ds@gmail.com>
2025-06-25 17:51:10 +0530

    first commit

---
+++ tech/src/model_training.py
@@ -0,0 +1,7 @@
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_absolute_percentage_error, mean_squared_error
+import numpy as np 
+import os  
+import pandas as pd 
+
