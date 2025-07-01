362d95451b4f7d5a30622b8b69b2531cdffb8d19
Himanshu Verma <himanshu.verma.ds@gmail.com>
2025-07-01 20:47:01 +0530

    added code for hyperparameter tuning via optuna

--- tech/src/model_training.py
+++ tech/src/model_training.py
@@ -9,6 +9,7 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader 
 from torch.utils.tensorboard import SummaryWriter
+import matplotlib.pyplot as plt
 
 
 class Model(nn.Module):
@@ -38,9 +39,23 @@ class Model(nn.Module):
         return x
             
             
-X,y= make_regression(n_samples=5000, n_features= 10, noise= 0.5, random_state= 42)
+# X,y= make_regression(n_samples=5000, n_features= 10, noise= 0.001, random_state= 42)
+
+# Making the dataset
+n_samples = 5000
+n_features = 10
+noise_factor= 0.001
+
+X = torch.randn(n_samples, n_features)
+true_weights = torch.tensor([3.0, -2.5, 0.0, 0.0, 5.0, 0.0, 1.2, 0.0, 0.0, -4.0]) 
+noise = torch.randn(n_samples) * noise_factor
+
+y = torch.matmul(X, true_weights) + noise
+y = y.view(-1)    
+
 df= pd.DataFrame(X, columns= [f'feature_{i}' for i in range(X.shape[1])])
 df['target']= y
+print(df.head(10))
 
 
 X_train, X_test, y_train, y_test= train_test_split(X, y, test_size=0.2, random_state=42)
@@ -64,18 +79,12 @@ class CustomDataset(Dataset):
         return self.features[idx], self.target[idx]
         
 
-
-
 # Creating dataloaders
 train_dataset= CustomDataset(X_train, y_train)
 test_dataset= CustomDataset(X_test, y_test)
 
 train_dataloader= DataLoader(train_dataset, shuffle= True, batch_size=6)
 test_dataloader= DataLoader(test_dataset, shuffle= False, batch_size= 6)
-
-
-print('Train dataloader: ', train_dataloader)
-print('Test dataloader: ', test_dataloader)
 
 
 # Training loop
@@ -85,20 +94,21 @@ model= Model(input_features= X_train.shape[1],
 
 model.to(device= device)
 
-print(model)
 loss_fn= nn.MSELoss()
-optimizer= torch.optim.Adam(model.parameters(), lr= 3e-9)
+optimizer= torch.optim.Adam(model.parameters(), lr= 3e-3)
 writer= SummaryWriter()
 
-epochs= 100
+epochs= 200
+per_epoch_loss_list= []
 for epoch in range(epochs):
     
+    model.train()
     total_epoch_loss= 0
     for batch_features, batch_target in train_dataloader:
         
         batch_features, batch_target= batch_features.to(device), batch_target.to(device)
         y_pred= model(batch_features)
-        loss= loss_fn(y_pred, batch_target.view(-1))
+        loss= loss_fn(y_pred.view(-1), batch_target.view(-1))
         
         optimizer.zero_grad()
         loss.backward()
@@ -106,9 +116,19 @@ for epoch in range(epochs):
         
         total_epoch_loss += loss.item()
     avg_loss= total_epoch_loss/len(train_dataloader)
+    per_epoch_loss_list.append(avg_loss)
     writer.add_scalar('Training Loss', avg_loss, epoch)
     
     print(f'Epoch: {epoch+1}, Avg. loss: {avg_loss}')
+    
+plt.plot(per_epoch_loss_list)
+plt.title('Average training loss')
+plt.ylabel('Avg. train loss')
+plt.xlabel('Epochs')
+plt.grid()
+plt.savefig('Train loss curve.png')
+print('plot saved !!')
+plt.close()
         
 
 # Evaluating the model
@@ -130,7 +150,5 @@ all_targets= np.concatenate(all_targets)
 mse= mean_squared_error(all_preds, all_targets)
 mape= mean_absolute_percentage_error(all_preds, all_targets)
 
-print('Actuals: ', all_targets)
-print('Predictions: ', all_preds)
 print('MSE: ', mse)
-print('MAPE: ', mape)    
+print('MAPE: ', mape)  
---
+++ tech/src/model_training_optuna.py
@@ -0,0 +1,166 @@
+import pandas as pd 
+import numpy as np 
+from sklearn.datasets import make_regression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import torch 
+import torch.nn as nn 
+from torch.utils.data import Dataset, DataLoader
+import optuna 
+
+
+class MyModel(nn.Module):
+    def __init__(self, input_features, hidden_neurons, output_features):
+        super().__init__()
+        self.input_features= input_features
+        self.hidden_neurons= hidden_neurons
+        self.output_features= output_features
+        self.linear1= nn.Linear(input_features, hidden_neurons)
+        self.relu= nn.ReLU()
+        self.linear2= nn.Linear(hidden_neurons, hidden_neurons)
+        self.output_layer= nn.Linear(hidden_neurons, output_features)
+        
+    def forward(self, x):
+        x= self.linear1(x)
+        x= self.relu(x)
+        x= self.linear2(x)
+        x= self.relu(x)
+        x= self.output_layer(x)
+        return x
+    
+
+class CustomDataset(Dataset):
+    def __init__(self, features, target):
+        self.features= torch.tensor(features, dtype= torch.float32)
+        self.target= torch.tensor(target, dtype= torch.float32)
+        
+    def __len__(self):
+        return self.features.shape[0]
+    
+    def __getitem__(self, idx):
+        return self.features[idx], self.target[idx]
+    
+    
+    
+def train_and_validate(model, train_dataloader, val_dataloader, epochs, optimizer, loss_fn, device):
+    
+    model= model.train()
+    model= model.to(device)
+    for epoch in range(epochs):
+        total_epoch_loss= 0
+        
+        for batch_features, batch_target in train_dataloader:
+            batch_features, batch_target= batch_features.to(device), batch_target.to(device)
+            
+            y_pred= model(batch_features)
+            loss= loss_fn(y_pred, batch_target.view(-1))
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+            total_epoch_loss += loss.item()
+        
+        avg_train_loss= total_epoch_loss/len(train_dataloader)
+        print(f'Epoch: {epoch+1}, Avg. loss per epoch: {avg_train_loss}')
+    
+    # Eval mode
+    model.eval()
+    val_loss=0
+    with torch.no_grad():
+        for batch_features, batch_target in val_dataloader:
+            preds= model(batch_features)
+            loss= loss_fn(preds, batch_target.view(-1))
+            val_loss += loss.item()
+    
+    avg_val_loss= val_loss/len(val_dataloader)
+    print('Average validation loss: ', avg_train_loss)
+    return avg_val_loss
+        
+    
+
+def evaluate_on_test(model, test_dataloader, device):
+    
+    model= model.to(device)
+    loss_fn= nn.MSELoss()
+    
+    model.eval()
+    test_loss= 0
+    with torch.no_grad():
+        for batch_features, batch_target in test_dataloader:
+            batch_features, batch_target= batch_features.to(device), batch_target.to(device)
+            preds= model(batch_features)
+            loss= loss_fn(preds, batch_target.view(-1))
+            test_loss += loss.item()
+    
+    avg_test_loss= test_loss/len(test_dataloader)
+    return avg_test_loss
+
+
+
+test_dataloader_global = None
+X_test_global= None 
+y_test_global= None 
+
+def objective(trial):
+    
+    hidden_neurons= trial.suggest_int('hidden_neurons', 16, 256, step= 16)
+    lr= trial.suggest_float('lr', 1e-9, 1e-2, log= True)
+    batch_size = trial.suggest_categorical("batch_size", [16, 32, 64, 128])
+    
+    # train test split 
+    X, y= make_regression(n_samples= 10000, n_features=5, noise= 0.1, random_state= 42)
+    df= pd.DataFrame(X, columns= [f'feature_{i}' for i in range(X.shape[1])])
+    df['target']= y
+    
+    X_tmp, X_test, y_tmp, y_test= train_test_split(X, y, test_size= 0.2, random_state= 42)
+    X_train, X_val, y_train, y_val= train_test_split(X_tmp, y_tmp, test_size= 0.2, random_state= 42)
+    
+    train_dataloader= DataLoader(CustomDataset(X_train, y_train), batch_size= batch_size, shuffle= True)
+    val_dataloader= DataLoader(CustomDataset(X_val, y_val), batch_size= batch_size, shuffle= False)
+    test_dataloader= DataLoader(CustomDataset(X_test, y_test))
+    
+    
+    if test_dataloader_global is None:
+        test_dataloader_global= test_dataloader
+    
+    model= MyModel(input_features= X_train.shape[1],
+                   hidden_neurons= hidden_neurons,
+                   output_features= 1)
+    
+    if X_test_global is None:
+        X_test_global= X_test 
+    
+    if y_test_global is None:
+        y_test_global= y_test
+    
+    
+    epochs= 50
+    loss_fn= nn.MSELoss()
+    optimizer= torch.optim.Adam(model.parameters(), lr= lr)
+    device= torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    
+    val_loss= train_and_validate(model= model,
+                                 train_dataloader= train_dataloader,
+                                 val_dataloader= val_dataloader,
+                                 epochs= epochs,
+                                 optimizer= optimizer,
+                                 loss_fn= loss_fn,
+                                 device= device)
+    
+    return val_loss
+    
+
+
+if __name__ == '__main__':
+    study= optuna.create_study(direction= 'minimize')
+    study.optimize(objective, n_trials= 30)
+    
+    print("\nBest Trial:")
+    print(f"Validation Loss: {study.best_value:.4f}")
+    print(f"Best Params: {study.best_params}")
+    
+    # Predicting with the best parameters model
+
+    model= MyModel(input_features= X_test_global.shape[1],
+                   )       
+    
